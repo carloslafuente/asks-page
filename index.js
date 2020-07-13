4d13e2af3726923da3ed0b89e1b300129209ef1c
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-07-13 18:38:47 -0400

    deploy

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const blankie = require('blankie');
 const server = hapi.server({
   autoListen: true,
   port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST || '0.0.0.0',
   routes: {
     files: {
       relativeTo: path.join(__dirname, 'public'),
